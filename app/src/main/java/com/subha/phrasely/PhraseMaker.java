ac026c25fcaf2a26c711a85f1e9a86ab3d0cf3ae
Subha Azrin <subhaazrin@gmail.com>
2021-03-22 01:19:40 -0400

    Nav bar added, tiny fixes yet to be made

--- app/src/main/java/com/subha/phrasely/PhraseMaker.java
+++ app/src/main/java/com/subha/phrasely/PhraseMaker.java
@@ -1,8 +1,13 @@
 package com.subha.phrasely;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PhraseMaker extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class PhraseMaker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrase_maker);
+
+        //initialize and assigning variable for nav bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //set home selected for this page
+        bottomNavigationView.setSelectedItemId(R.id.sentence_maker_nav);
+
+        //perfom ItemSelectedListener Event
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home_nav:
+                        startActivity(new Intent(getApplicationContext()
+                                ,HomePage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.lang_bot_nav:
+                        startActivity(new Intent(getApplicationContext()
+                                ,LanguageBot.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.sentence_maker_nav:
+                        return true;
+
+                    case R.id.settings_nav:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+
+
+        });
     }
 }
--- app/src/main/java/com/subha/phrasely/Settings.java
+++ app/src/main/java/com/subha/phrasely/Settings.java
@@ -6,22 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomePage extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_settings);
 
         //initialize and assigning variable for nav bar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
         //set home selected for this page
-        bottomNavigationView.setSelectedItemId(R.id.home_nav);
+        bottomNavigationView.setSelectedItemId(R.id.settings_nav);
 
         //perfom ItemSelectedListener Event
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
@@ -41,12 +40,12 @@ public class HomePage extends AppCompatActivity {
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.home_nav:
+                    case R.id.settings_nav:
                         return true;
 
-                    case R.id.settings_nav:
+                    case R.id.home_nav:
                         startActivity(new Intent(getApplicationContext()
-                                ,Settings.class));
+                                ,HomePage.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
@@ -56,21 +55,4 @@ public class HomePage extends AppCompatActivity {
 
         });
     }
-
-    //method for going to PhraseMaker
-    public void goPhraseMaker(View v){
-        //launching Phrase Maker activity
-        Intent phrasemaker = new Intent (this, PhraseMaker.class);
-        startActivity(phrasemaker);
-
-    }
-
-    //method for going to LanguageBot
-    public void goLanguageBot(View v){
-        //launching Language Bot activity
-        Intent langbott = new Intent (this, LanguageBot.class);
-        startActivity(langbott);
-
-    }
-
 }
